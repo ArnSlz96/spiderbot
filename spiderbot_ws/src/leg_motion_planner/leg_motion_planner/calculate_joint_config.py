c53bc4ad2a9dddaeaf13909af106eb10b1b1bc4a
Arne Schulz <arne-schulz2@gmx.de>
2025-02-20 18:10:00 +0100

    Add forward kinematics calculation method to joint_angle_calculator class.

--- spiderbot_ws/src/leg_motion_planner/leg_motion_planner/calculate_joint_config.py
+++ spiderbot_ws/src/leg_motion_planner/leg_motion_planner/calculate_joint_config.py
@@ -1,7 +1,9 @@
 import math
+import numpy as np
+from scipy.spatial.transform import Rotation
 import rclpy
 from geometry_msgs.msg import Point
-from mechanical.mechanical import load_mechanical_params
+from mechanical.load_mechanical_params import load_mechanical_params
 
 class joint_angle_calculator():
     def __init__(self):
@@ -10,10 +12,13 @@ class joint_angle_calculator():
 
         # Get mechanical parameters from mechanical module
         mech_params = load_mechanical_params()
+        self.LEG_SEGMENT1_LENGTH = mech_params["LEG_SEGMENT1_LENGTH"]
+        self.LEG_SEGMENT2_LENGTH = mech_params["LEG_SEGMENT2_LENGTH"]
 
-        LEG_SEGMENT1_LENGTH = mech_params["LEG_SEGMENT1_LENGTH"]
-        LEG_SEGMENT2_LENGTH = mech_params["LEG_SEGMENT2_LENGTH"]
-        self.LEG_LENGTH = LEG_SEGMENT1_LENGTH + LEG_SEGMENT2_LENGTH
+        # Threshold for difference between wanted end_pos and resulting end_pos from joint angles
+        # for joint angles to be seen as valid (in mm)
+        self.max_valid_endpos_distance =  5
+
 
     # --- Primary usage function
 
@@ -22,26 +27,67 @@ class joint_angle_calculator():
             self.roslog.warning(f"Received invalid end_pos {end_pos}")
 
         # Do calc here
-        # Yaw should be easy
+        hip_yaw = self._calc_hip_yaw(end_pos)
+        hip_pitch, knee_pitch = self._calc_hip_knee_pitches(end_pos)
 
-        # Pitches a bit more involved
+        # Do validity check for end_pos resulting from calculated joint angles
+        resulting_end_pos = self._get_endpos_from_joint_angles(hip_yaw, hip_pitch, knee_pitch)
 
-        # TODO figure out geometric relations
+        # Check calculated joint configs against range of valid joint configs
 
         return [hip_yaw, hip_pitch, knee_pitch]
     
     # --- Helper functions
 
     def _is_valid_end_pos(self, end_pos: Point) -> bool:
+        total_leg_length = self.LEG_SEGMENT1_LENGTH + self.LEG_SEGMENT2_LENGTH
+
         # Check for correct input   
-        if Point.z>0:
+        if end_pos.z>0:
             self.roslog.warning(f"Unexpected Z-value end_pos for joint_angle planner. Should be negative, received {Point.z}.")
             return False
         
         # Check if end_pos is outside of leg range
         distance_from_leg_origin = np.linalg.norm([end_pos.x, end_pos.y, end_pos.z])
-        if  distance_from_leg_origin > self.LEG_LENGTH:
-            self.roslog.warning(f"end_pos outside of leg range. Should be less than {self.LEG_LENGTH}, is {distance_from_leg_origin}")
+        if  distance_from_leg_origin > total_leg_length:
+            self.roslog.warning(f"end_pos outside of leg range. Should be less than {total_leg_length}, is {distance_from_leg_origin}")
             return False
         
         return True
+
+    # Calculate hip yaw in x-y plane
+    def _calc_hip_yaw(self, end_pos: Point) -> float:
+        hip_yaw = math.atan(end_pos.y / end_pos.x)
+        return hip_yaw
+
+    def _get_endpos_from_joint_angles(self, hip_yaw: float, hip_pitch: float, knee_pitch:float) -> Point:
+        # Define all affine transformation matrices
+        hip_yaw_rotation = Rotation.from_euler('z', hip_yaw).as_matrix()
+        hip_pitch_rotation = Rotation.from_euler('y', hip_pitch).as_matrix()
+        knee_pitch_rotation = Rotation.from_euler('y', knee_pitch).as_matrix()
+
+        T_hip_yaw = np.eye(4)
+        T_hip_yaw[:3, :3] = hip_yaw_rotation
+
+        T_hip_pitch = np.eye(4)
+        T_hip_pitch[:3, :3] = hip_pitch_rotation
+
+        T_seg1 = np.eye(4)
+        T_seg1[0, 3] = self.LEG_SEGMENT1_LENGTH
+
+        T_knee_pitch = np.eye(4)
+        T_knee_pitch[:3, :3] = knee_pitch_rotation
+
+        T_seg2 = np.eye(4)
+        T_seg2[0, 3] = self.LEG_SEGMENT2_LENGTH
+
+        # Chain transformations
+        T = np.eye(4)
+        T = T @ T_hip_yaw @ T_hip_pitch @ T_seg1
+        T = T @ T_knee_pitch @ T_seg2
+
+        end_position = T[:3, 3]
+        end_orientation = T[:3, :3]
+
+        return end_position
+
